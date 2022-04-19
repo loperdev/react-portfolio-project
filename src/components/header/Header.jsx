98bba500b6ff463b7a354335379f6b27322033ec
Dev Vyaas <devendravyas04@gmail.com>
2022-04-19 21:00:47 +0100

    Header component finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import Headersocial from './Headersocials'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Dev Vyaas</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+        <Headersocial />
+
+
+        <div className="me">
+          <img src={ME} alt="me"  />
+        </div>
+
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+
+      </div>
+    </header>
   )
 }
 
